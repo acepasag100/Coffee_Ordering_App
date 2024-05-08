946b424bff7924cfdada21325d86e4d8ce7a6955
Ace Pasag <acepasag100@outlook.com>
2024-05-08 08:00:04 +0800

    Update

--- app/src/main/java/com/example/coffeeorderingapp/OrderAdapter.java
+++ app/src/main/java/com/example/coffeeorderingapp/OrderAdapter.java
@@ -1,17 +1,31 @@
 package com.example.coffeeorderingapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder>{
 
-    @NonNull
+    List<Model> modelList;
+    Context context;
+
+    public OrderAdapter(List<Model> modelList, Context context) {
+        this.modelList = modelList;
+        this.context = context;
+    }
+
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.listitem, parent, false);
+        return new ViewHolder(view);
+
     }
 
     @Override
@@ -20,11 +34,13 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder>{
     }
 
     @Override
-    public int getItemCount() {
-        return 0;
+    public int getItemCount()
+    {
+        return modelList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
