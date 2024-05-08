2832f46c157c360e86726e7100ed004a4e10aa62
Ace Pasag <acepasag100@outlook.com>
2024-05-08 08:43:56 +0800

    Fix OrderAdapter.java

--- app/src/main/java/com/example/coffeeorderingapp/MainActivity.java
+++ app/src/main/java/com/example/coffeeorderingapp/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.coffeeorderingapp;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<Model> modelList;
+    RecyclerView recyclerView;
+    OrderAdapter orderAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        modelList = new ArrayList<>();
+        modelList.add(new Model("Green Tea", getString(R.string.greentea), R.drawable.greentea ));
+        modelList.add(new Model("Latte", getString(R.string.latte), R.drawable.late));
+        modelList.add(new Model("Orange Smoothie", getString(R.string.orangesmoothie), R.drawable.orange));
+        modelList.add(new Model("Orange Vanilla", getString(R.string.orangevanilla), R.drawable.orangevanilla));
+        modelList.add(new Model("Cappucino", getString(R.string.cappcuni), R.drawable.cappcunio));
+        modelList.add(new Model("Thai Tea", getString(R.string.thaitea), R.drawable.thaitea));
+        modelList.add(new Model("Tea", getString(R.string.tea), R.drawable.tea));
+        modelList.add(new Model("Bubble Tea", getString(R.string.bubbletea), R.drawable.milk));
+        modelList.add(new Model("Matcha", getString(R.string.match), R.drawable.match));
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(null));
+
+        orderAdapter = new OrderAdapter(modelList, this);
+        recyclerView.setAdapter(orderAdapter);
+
     }
 }
--- app/src/main/java/com/example/coffeeorderingapp/OrderAdapter.java
+++ app/src/main/java/com/example/coffeeorderingapp/OrderAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -29,8 +31,14 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder>{
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String getCoffeeName = modelList.get(position).getmDrinkName();
+        String getDescription = modelList.get(position).getmDrinkDetail();
+        int getImage = modelList.get(position).getmDrinkPhoto();
 
+        holder.coffeeName.setText(getCoffeeName);
+        holder.coffeeDescription.setText(getDescription);
+        holder.coffeeImage.setImageResource(getImage);
     }
 
     @Override
@@ -41,9 +49,14 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder>{
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        TextView coffeeName, coffeeDescription;
+        ImageView coffeeImage;
 
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder(View itemView) {
             super(itemView);
+            coffeeName = itemView.findViewById(R.id.coffeeName);
+            coffeeDescription = itemView.findViewById(R.id.coffeeDescription);
+            coffeeImage = itemView.findViewById(R.id.coffeeImage);
         }
     }
 }
